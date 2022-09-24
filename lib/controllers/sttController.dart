c76419e465ed2a8e737b00a8d23993ebc0b136a9
Emir <32276305+MrEmii@users.noreply.github.com>
2022-09-24 19:30:54 -0300

    Update sttController.dart

--- lib/controllers/sttController.dart
+++ lib/controllers/sttController.dart
@@ -48,7 +48,10 @@ class SttController extends GetxController {
 
     await _recorder.start();
     recognizing.value = true;
-    final serviceAccount = ServiceAccount.fromString(r'''{
+    
+    final clientX59 = dotenv.env['APP_ID'] ?? 'add Proper Values';
+    
+    final ServiceAccount serviceAccount = ServiceAccount.fromString({
       "type": "service_account",
       "project_id": dotenv.env['PROJECT_ID'] ?? 'add Proper Values',
       "private_key_id": dotenv.env['PRIVATE_KEY_ID_2'] ?? 'add Proper Values',
@@ -59,7 +62,8 @@ class SttController extends GetxController {
       "token_uri": "https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
       "client_x509_cert_url": clientX59,
-    ''');
+    }.toString());
+
     final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
     final config = _getConfig();
 
