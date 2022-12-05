78b82520a0e61d882f63684cfc91db3b271a0050
Emir <emirhassan.ali02@gmail.com>
2022-12-05 14:51:15 -0300

    feat: added firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,14 @@ import 'package:questions_by_ottaa/application/application.dart';
 import 'package:questions_by_ottaa/application/injector.dart';
 import 'package:questions_by_ottaa/application/services/service_locator.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   await dotenv.load(fileName: "dotenv");
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  kIsWeb
-      ? await Firebase.initializeApp(
-          options: FirebaseOptions(
-            apiKey: dotenv.env['API_KEY'] ?? 'add Proper Values',
-            authDomain: dotenv.env['AUTH_DOMAIN'] ?? 'add Proper Values',
-            databaseURL: dotenv.env['DATA_BASE_URL'] ?? 'add Proper Values',
-            projectId: dotenv.env['PROJECT_ID'] ?? 'add Proper Values',
-            storageBucket: dotenv.env['STORAGE_BUCKET'] ?? 'add Proper Values',
-            messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'] ?? 'add Proper Values',
-            appId: dotenv.env['APP_ID'] ?? 'add Proper Values',
-            measurementId: dotenv.env['MEASUREMENT_ID'] ?? 'add Proper Values',
-          ),
-        )
-      : await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
 
